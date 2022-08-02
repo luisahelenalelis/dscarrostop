05c99119aa326afc5a861aa69dabc6111dcfc962
Luisa Helena <luisa.lelis@sga.pucminas.br>
2022-08-02 17:28:08 +0100

    Navbar

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,8 +1,14 @@
+import './assets/styles/custom.scss';
 import './App.css';
+
+import Navbar from 'components/Navbar';
 
 function App() {
   return (
-    <h1>Ds Carros Top</h1>
+    <>
+      <Navbar />
+      <h1>Ds Carros Top</h1>
+    </>
   );
 }
 
